8265e1ca5706e06e277b6984989dd5742b89ea60
Sloane Wolter <sloanewolter@gmail.com>
2023-07-15 21:16:27 -0500

    added banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner.js";
 
 
 function App() {
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
