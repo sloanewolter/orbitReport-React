a36f553963e9348f74989d4fc8b1ad056411b406
Sloane Wolter <sloanewolter@gmail.com>
2023-07-18 21:32:57 -0500

    got buttons to show up properly on the page

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,18 +1,19 @@
 import satData from "./satData";
 
 
-const Buttons = (filterByType, setSat, displaySats) => {
+const Buttons = ({filterByType, setSat, displaySats}) => {
   return (
     <div>
-    {displaySats.map((sat, id) => {
+  {displaySats.map((sat, id) => {
     return (
       <button onClick={() => filterByType(sat)} key={id}>
         {sat} Orbit
       </button>
     );
-    })};
-    <button onClick = { () => setSat(satData)} />
-    </div>
-  )}
+  })}
+  <button onClick={() => setSat(satData)} >All orbits</button>
+   </div> 
+  
+)};
 
 export default Buttons;
